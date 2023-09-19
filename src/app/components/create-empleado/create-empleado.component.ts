7f7bfd740c320159e6dca18ca4a5101e0409d486
quasarlights <gastonestebancampagno@gmail.com>
2023-09-19 15:37:11 -0300

    validations on create and update done

--- src/app/components/create-empleado/create-empleado.component.ts
+++ src/app/components/create-empleado/create-empleado.component.ts
@@ -36,25 +36,98 @@ export class CreateEmpleadoComponent {
     const letters = /^[A-Za-z ]*$/;
 
     if (!value.match(letters)) {
-      input.value = value.replace(/[^A-Za-z ]/g, ''); // Eliminar caracteres no válidos
-      this.mostrarMensajeNombre = input.id === 'name'; // Mostrar el mensaje de error según el campo
+      input.value = value.replace(/[^A-Za-z ]/g, ''); 
+      this.mostrarMensajeNombre = input.id === 'name'; 
       this.mostrarMensajeApellido = input.id === 'surname';
     } else {
-      this.mostrarMensajeNombre = false; // Ocultar el mensaje de error si la entrada es válida
+      this.mostrarMensajeNombre = false; 
       this.mostrarMensajeApellido = false;
     }
   }
 
   //VALIDACION EMAIL
-
- // Crea un FormGroup para el formulario
  formulario: FormGroup;
+ 
  constructor(private fb: FormBuilder, private empleadoService: EmpleadoService) {
-   // Define el formulario y aplica validadores
-   this.formulario = this.fb.group({
-     email: ['', [Validators.required, Validators.email]]
-   });
- }
+  
+  this.formulario = this.fb.group({
+    email: ['', [Validators.required, Validators.email]], 
+    admissionDay: ['', Validators.required],
+    birthDay: ['', Validators.required]
+  });
+}
+//VALIDAR FECHA NACIMIENTO
+/*maxDate = new Date();
+
+
+mostrarMensajeFechaNacimiento: boolean = false;
+
+validarFechaNacimiento(event:any): void {
+  const birthDateInput = new Date(this.birthDate);
+  const currentDate = new Date();
+  const edadMinima = 18;}*/
+/*
+birthDate: string = '';
+maxDate: string='';
+mostrarMensajeFechaNacimiento: boolean = false;
+currentDate = new Date();
+maxDateBirth = this.currentDate.toISOString().split('T')[0];
+*/
+maxDate: string = '';
+maxDateBirth: string = '';
+mostrarMensajeFechaNacimiento: boolean = false;
+
+validarFechaNacimiento(fechaNacimiento: string): void {
+  const birthDateInput = new Date(fechaNacimiento);
+  const currentDate = new Date();
+  const edadMinima = 18;
+
+  // Calcula la diferencia en años entre las fechas
+  let edad = currentDate.getFullYear() - birthDateInput.getFullYear();
+
+  // Comprueba si la fecha actual es anterior al cumpleaños de este año
+  const mesActual = currentDate.getMonth() + 1;
+  const diaActual = currentDate.getDate();
+  const mesNacimiento = birthDateInput.getMonth() + 1;
+  const diaNacimiento = birthDateInput.getDate();
+
+  if (
+    mesActual < mesNacimiento ||
+    (mesActual === mesNacimiento && diaActual < diaNacimiento)
+  ) {
+    edad--;
+  }
+
+  // Comprueba si la edad es menor que la edad mínima permitida usando la función adaptada
+  if (!this.isOverEighteen(birthDateInput.getFullYear(), mesNacimiento, diaNacimiento)) {
+    this.mostrarMensajeFechaNacimiento = true;
+  } else {
+    this.mostrarMensajeFechaNacimiento = false;
+  }
+}
+
+isOverEighteen(year: number, month: number, day: number): boolean {
+  const now = parseInt(new Date().toISOString().slice(0, 10).replace(/-/g, ''));
+  const dob = year * 10000 + month * 100 + day * 1;
+
+  return now - dob > 180000;
+}
+
+ //VALIDACION FECHA INGRESO
+
+  admissionDate: string = '';
+  mostrarMensajeFechaAdmision: boolean = false;
+
+  validarFechaAdmision(): void {
+    const admissionDateInput = new Date(this.admissionDate);
+    const currentDate = new Date();
+
+    if (admissionDateInput > currentDate) {
+      this.mostrarMensajeFechaAdmision = true;
+    } else {
+      this.mostrarMensajeFechaAdmision = false;
+    }
+  }
   //ENVIAR REQUEST PARA CREAR EMPLEADO
   mensaje: string = '';
   crearEmpleado(): void {
@@ -88,30 +161,4 @@ export class CreateEmpleadoComponent {
       alert('Por favor, ingrese un correo electrónico válido.');
     }
   }
-  
-  /*
-  crearEmpleado(): void {
-    const nuevoEmpleado: Empleado = {
-      id: 0, 
-      nroDocumento: +(<HTMLInputElement>document.getElementById('nroDocumento')).value,
-      nombre: (<HTMLInputElement>document.getElementById('name')).value,
-      apellido: (<HTMLInputElement>document.getElementById('surname')).value,
-      email: (<HTMLInputElement>document.getElementById('exampleInputEmail1')).value,
-      fechaNacimiento: (<HTMLInputElement>document.getElementById('birthDay')).value,
-      fechaIngreso: (<HTMLInputElement>document.getElementById('admissionDay')).value,
-      fechaCreacion: ''
-    };
-
-    this.empleadoService.createEmpleado(nuevoEmpleado).subscribe(
-      (empleadoCreado) => {
-        this.mensaje = 'Empleado '+empleadoCreado.nombre+' '+empleadoCreado.apellido+' creado exitosamente';
-        alert(this.mensaje);
-      },
-      (error) => {
-        this.mensaje = 'Error al crear el empleado: ' + error.error.message;
-        alert(this.mensaje);
-      }
-    );
-  }*/
-
 }
--- src/app/components/update-empleado/update-empleado.component.ts
+++ src/app/components/update-empleado/update-empleado.component.ts
@@ -8,7 +8,9 @@ import { EmpleadoService } from 'src/app/services/empleado.service';
   styleUrls: ['./update-empleado.component.css']
 })
 export class UpdateEmpleadoComponent implements OnInit{
+
   empleados:Empleado[]=[];
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   empleadoSeleccionado: Empleado= 
   {
     id: 0,
@@ -22,15 +24,17 @@ export class UpdateEmpleadoComponent implements OnInit{
   };
   empleadoIdSeleccionado: number= 0; // Almacena ID del empleado seleccionado
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
 
-  constructor(private empleadoService: EmpleadoService){}
+  constructor(private empleadoService: EmpleadoService,){}
 
   ngOnInit(): void {
       this.empleadoService.getEmpleados().subscribe(data=>{
         this.empleados=data;})
   };
 
-  // Carga la info del empleado cuando se selecciona un empleado
+// Carga la info del empleado cuando se selecciona un empleado
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   cargarEmpleado() {
     if (this.empleadoIdSeleccionado) {
       this.empleadoService.getEmpleadoPorId(this.empleadoIdSeleccionado).subscribe(empleado => {
@@ -38,7 +42,110 @@ export class UpdateEmpleadoComponent implements OnInit{
       });
     }
   }
+//VALIDACION DNI
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+mostrarMensaje: boolean = false;
 
+soloNumeros(event:any): void {
+  const value = this.empleadoSeleccionado.nroDocumento;
+
+  if (!Number.isInteger(value)) {
+    this.mostrarMensaje = true;
+  } else {
+    this.mostrarMensaje = false;
+  }
+}
+
+  //VALIDACIONES NOMBRE Y APELLIDO
+  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  mostrarMensajeNombre: boolean = false; 
+  mostrarMensajeApellido: boolean = false; 
+
+  soloLetras(event: any): void {
+    const input = event.target;
+    const value = input.value;
+    const letters = /^[A-Za-z ]*$/;
+
+    if (!value.match(letters)) {
+      input.value = value.replace(/[^A-Za-z ]/g, '');
+      this.mostrarMensajeNombre = input.id === 'name';
+      this.mostrarMensajeApellido = input.id === 'surname';
+    } else {
+      this.mostrarMensajeNombre = false;
+      this.mostrarMensajeApellido = false;
+    }
+  }
+
+  //VALIDAR EMAIL
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  validarEmail(email: string): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
+  }
+
+  //VALIDAR FECHA DE NACIMIENTO
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+maxDate: string='';
+mostrarMensajeFechaNacimiento: boolean = false;
+currentDate = new Date();
+maxDateBirth = this.currentDate.toISOString().split('T')[0];
+
+validarFechaNacimiento(fechaNacimiento: string): void {
+  const birthDateInput = new Date(fechaNacimiento);
+  const currentDate = new Date();
+  const edadMinima = 18;
+
+  // Calcula la diferencia en años entre las fechas
+  let edad = currentDate.getFullYear() - birthDateInput.getFullYear();
+
+  // Comprueba si la fecha actual es anterior al cumpleaños de este año
+  const mesActual = currentDate.getMonth() + 1;
+  const diaActual = currentDate.getDate();
+  const mesNacimiento = birthDateInput.getMonth() + 1;
+  const diaNacimiento = birthDateInput.getDate();
+
+  if (
+    mesActual < mesNacimiento ||
+    (mesActual === mesNacimiento && diaActual < diaNacimiento)
+  ) {
+    edad--;
+  }
+
+  // Comprueba si la edad es menor que la edad mínima permitida usando la función adaptada
+  if (!this.isOverEighteen(birthDateInput.getFullYear(), mesNacimiento, diaNacimiento)) {
+    this.mostrarMensajeFechaNacimiento = true;
+  } else {
+    this.mostrarMensajeFechaNacimiento = false;
+  }
+}
+
+isOverEighteen(year: number, month: number, day: number): boolean {
+  const now = parseInt(new Date().toISOString().slice(0, 10).replace(/-/g, ''));
+  const dob = year * 10000 + month * 100 + day * 1;
+
+  return now - dob > 180000;
+}
+
+ //VALIDACION FECHA INGRESO
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+ admissionDate: string = '';
+ mostrarMensajeFechaAdmision: boolean = false;
+
+ validarFechaAdmision(event: any): void {
+   const admissionDateInput = new Date(this.admissionDate);
+   const currentDate = new Date();
+
+   if (admissionDateInput > currentDate) {
+     this.mostrarMensajeFechaAdmision = true;
+   } else {
+     this.mostrarMensajeFechaAdmision = false;
+   }
+ }
+
+//FETCH API
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   actualizarEmpleado() {
     this.empleadoService.updateEmpleado(this.empleadoIdSeleccionado, this.empleadoSeleccionado)
       .subscribe(
